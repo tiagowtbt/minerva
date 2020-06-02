8e2503c363aee105f85b3646f6b018ea4ba1f9ce
tiagowtbt <tiagowtbt@gmail.com>
2020-06-02 14:36:09 -0300

    Routes

---
+++ lib/services/routes.dart
@@ -0,0 +1,14 @@
+import 'package:minerva/pages/userpage.dart';
+import 'package:minerva/pages/config.dart';
+import 'package:minerva/pages/help.dart';
+import 'package:minerva/pages/notifications.dart';
+import 'package:minerva/pages/security.dart';
+
+class Routes {
+  static const String userpage = Userpage.routeName;
+  static const String config = Config.routeName;
+  static const String notifications = Notifications.routeName;
+  static const String security = Security.routeName;
+  static const String help = Help.routeName;
+
+}
